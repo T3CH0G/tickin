ab97e3c9da0464f24908fe17b5cc4b1ffa29747a
T3CH0G <benphang1234@gmail.com>
2018-04-14 13:22:01 -0700

    songs

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Songs.create(name: "Faded - Alan Walker", score: 0)
-Songs.create(name: "Hymn For the Weekend - Coldplay", score: 0)
-Songs.create(name: "Despacito - Luis Fonsi, Daddy", score: 0)
-Songs.create(name: "Stranger Things - Kygo, Onerepublic", score: 0)
+Song.create(name: "Faded - Alan Walker", score: 0)
+Song.create(name: "Hymn For the Weekend - Coldplay", score: 0)
+Song.create(name: "Despacito - Luis Fonsi, Daddy", score: 0)
+Song.create(name: "Stranger Things - Kygo, Onerepublic", score: 0)
 
