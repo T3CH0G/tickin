07b9157cfb24149b0fe3434f79e9b3d7a5817fc4
T3CH0G <benphang1234@gmail.com>
2018-04-14 13:17:55 -0700

    heroku

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Songs.create(name: "Faded - Alan Walker", score: 0)
+Songs.create(name: "Hymn For the Weekend - Coldplay", score: 0)
+Songs.create(name: "Despacito - Luis Fonsi, Daddy", score: 0)
+Songs.create(name: "Stranger Things - Kygo, Onerepublic", score: 0)
+
