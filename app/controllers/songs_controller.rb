e2ffa6ca2b3a88d56bae251183023ba1b46aa3e9
T3CH0G <benphang1234@gmail.com>
2018-04-14 13:40:29 -0700

    songs

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -10,7 +10,7 @@ class SongsController < ApplicationController
           s.delete
       end
     end
-    @songs = Song.all(:order => "created_at DESC")
+    @songs = Song.all.order('created_at DESC')
   end
 
   # GET /songs/1
